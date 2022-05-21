39eb5eea32e3ab221ed9c254227d19806c41d901
Dhruvil Patel <dhruvilpatel2002@gmail.com>
2022-05-21 17:28:29 +0530

    buy sell interface made and DBconnectivity done

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const multer = require('multer');
-var fs = require('fs');
+/* var fs = require('fs');
 var path = require('path');
-require('dotenv/config');
+require('dotenv/config'); */
 const { buyerUser, farmerUser } = require('../model/credentials');
 const { crop } = require('../model/crops');
 const router = express.Router();
 
-router.get('/signin', (req, res) => {
-    res.render('signin.ejs', { title: 'Sign In', alrt: '' });
+router.get('/', (req, res) => {
+    res.render('home.ejs', { title: 'Horizon', alrt: '' });
 }
 );
 router.get('/sign-up/:value', (req, res) => {
@@ -16,20 +16,10 @@ router.get('/sign-up/:value', (req, res) => {
     res.render('signup.ejs', { title: 'Sign Up', alrt: '', value: val });
 }
 );
-router.post('/createseller', (req, res) => {
-    const selleruser = new farmerUser(req.body);
-    selleruser
-        .save()
-        .then((result) => {
-            res.render("signin", {
-                title: "Horizon",
-                alrt: "User Created Successfully",
-            });
-        })
-        .catch((err) => {
-            res.render("404", { title: "404 Error" });
-        });
-});
+router.get('/signin', (req, res) => {
+    res.render('signin.ejs', { title: 'Sign In', alrt: '' });
+}
+);
 router.post('/createbuyer', (req, res) => {
     const buyeruser = new buyerUser(req.body);
     buyeruser
@@ -44,18 +34,20 @@ router.post('/createbuyer', (req, res) => {
             res.render("404", { title: "404 Error" });
         });
 });
-router.get('/reset', (req, res) => {
-    res.render('reset.ejs', { title: 'Reset Password', alrt: '' });
-}
-);
-router.get('/', (req, res) => {
-    res.render('home.ejs', { title: 'Horizon', alrt: '' });
-}
-);
-router.get('/about', (req, res) => {
-    res.render('about.ejs', { title: 'About', alrt: '' });
-}
-);
+router.post('/createseller', (req, res) => {
+    const selleruser = new farmerUser(req.body);
+    selleruser
+        .save()
+        .then((result) => {
+            res.render("signin", {
+                title: "Horizon",
+                alrt: "User Created Successfully",
+            });
+        })
+        .catch((err) => {
+            res.render("404", { title: "404 Error" });
+        });
+});
 router.get('/login/', async (req, res) => {
     const name = req.query.search1;
     const pass = req.query.search2;
@@ -71,11 +63,11 @@ router.get('/login/', async (req, res) => {
     }
     if (result != null) {
         if (ch == 0) {
-            crop.find({})
+            buyerUser.find({})
                 .sort({ createdAt: -1 })
                 .then(async (results) => {
-                    allcrops = await crop.find({});
-                    res.render("buy", { users: results, title: result.name, crops: allcrops });
+                    allcrops = await crop.find({}).sort({ name: -1 });
+                    res.render("buy", { users: results, title: result.name, crops: allcrops, alrt: '' });
                 })
                 .catch((err) => {
                     res.render("404", { title: "404 Error" });
@@ -85,7 +77,7 @@ router.get('/login/', async (req, res) => {
             farmerUser.find({})
                 .sort({ createdAt: -1 })
                 .then((results) => {
-                    res.render("sell", { users: results, title: result.name });
+                    res.render("sell", { users: results, title: result.name, alrt: '' });
                 })
                 .catch((err) => {
                     res.render("404", { title: "404 Error" });
@@ -95,8 +87,29 @@ router.get('/login/', async (req, res) => {
         res.render("signin", { title: "Sign In", alrt: "Invalid Credentials" });
     }
 });
+router.get('/reset', (req, res) => {
+    res.render('reset.ejs', { title: 'Reset Password', alrt: '' });
+}
+);
 router.post('/resetpass', async (req, res) => {
 });
+router.post('/post-crop', (req, res) => {
+    const postcrop = new crop(req.body);
+    postcrop
+        .save()
+        .then((result) => {
+            res.render("sell", { title: "Horizon", alrt: "Crop Posted Successfully" });
+        }
+        )
+        .catch((err) => {
+            res.render("404", { title: "404 Error hai" });
+        }
+        );
+});
+router.get('/about', (req, res) => {
+    res.render('about.ejs', { title: 'About', alrt: '' });
+}
+);
 router.use((req, res) => {
     res.render('404.ejs', { title: '404', alrt: '' });
 }
