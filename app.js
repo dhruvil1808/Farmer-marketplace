710f2871b0d3eaae8bc5db926ba37d3043bd4356
Dhruvil Patel <dhruvilpatel2002@gmail.com>
2022-06-15 14:39:33 +0530

    port settings changed

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const app = express();
 const appRoutes = require("./routes/routes");
 const DBURI =
     "mongodb+srv://dhruvil1808:Dhruvil1234@cluster0.6wkxh.mongodb.net/Horizon?retryWrites=true&w=majority";
-
+const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose
     .connect(DBURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(app.listen(3000))
+    .then(app.listen(port))
     .catch((err) => console.log(err));
 
 app.use(appRoutes);
